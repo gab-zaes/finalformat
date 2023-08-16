272597081cedc8ed03fa9fd8618626aec1a7f247
Gabriel Magalhães <gabm.motion@gmail.com>
2023-08-16 11:32:51 -0300

    Implementadas as funções read_file() e get_meta()

--- finalformat.py
+++ finalformat.py
@@ -1,10 +1,17 @@
 import mammoth
 import argparse
 import re
+import sys
 from fpdf import FPDF
 
 
+
 class Book():
+    """
+    The Book class holds a index of Chapter instances in a list.
+    It also holds extra information about the book, and the fonts that
+    are going to be used in pdf formatting later.
+    """
     def __init__(self, chapters: list, font1: str, font2: str, **meta):
         self.index = chapters
 
@@ -12,7 +19,7 @@ class Book():
         self.author = meta["author"]
         self.date = meta["date"]
         self.contact = meta["contact"]
-        self.copy = meta["copy"]
+        self.license = meta["license"]
         self.quote = meta["quote"]
         self.bio = meta["bio"]
 
@@ -21,18 +28,21 @@ class Book():
 
 
 class Chapter():
-    def __init__(self, name: str, body: str):
-        self._name = name
+    def __init__(self, title: str, body: str):
+        self.title = title
         self._body = body
+
+    def __str__(self):
+        return "\n    " + self._title + "\n\n    " + self._body
 
     # getter
     @property
-    def name(self):
-        return self._name
+    def title(self):
+        return self._title
 
-    @name.setter
-    def name(self, name):
-        self.name = name
+    @title.setter
+    def title(self, title):
+        self._title = title
 
     @property
     def body(self):
@@ -47,38 +57,99 @@ def main():
         epilog="Remember to input a docx or html file with a Title and Paragraphs sctructure!\n",
     )
 
-    #add arguments to parser
+    # add arguments to parser
     parser.add_argument("path", type=str, help="path to the file you want to format into pdf")
     parser.add_argument("-s", "--size", type=str, help="size of the page: expected Regular or Small. If ommitted, defaults to Regular")
     parser.add_argument("-f", "--font_body", type=int, help="Pick one of the available fonts. 0: Libre Baskerville; 1: PT Serif; 2: Lato; 3: Fira Sans; 4: Helvetica. Defaults to 0.")
     parser.add_argument("-t", "--font_title", type=int, help="0: <same font as font_body>; 1: Baskerville Old Face; 2: Raleway; 3: DM Sans; 4: Helvetica. Defaults to 1.")
 
-    #read the arguments into variable
+    # read the arguments into variable
     args = parser.parse_args()
-
-    print(args.path)
-
-
-    # call function to read html or docx file into memory
-    # returns list of Chapter objects
-
-
+   
+    try:
+        # returns list of Chapter objects
+        chapters = read_file(args.path)
+    except ValueError:
+        sys.exit("File extension not supported! Try html or docx file with title and paragraphs")
+        
     # call function to add metadata (title, author, year, contact info, copywright, quote, bio) prompting the user first
-    # returns Meta dictionary
+    meta = get_meta()
 
-
-    # call function to consolidate() the Book and Chapters structure
-    # returns the complete Book instance
-
+    # create Book object
+    book = Book(chapters, args.font_body, args.font_title, **meta)
 
     # ask the user if he/she wants to change the order of chapters. If so, call change_index() to sort chapters in Book instance
     # return another version of Book object
 
 
     # output the return value to consolidate() saving it to pdf by calling a function
+    # we must check for <em> and <strong> tags
+
     # returns FPDF object to be outputted
     return 0
+
+
+def read_file(path: str):
+    # variable for mammoth html convertion
+    style_map = """
+    p[style-name='Title'] => h1:fresh
+    p[style-name='Section Title'] => h1:fresh
+    p[style-name='Subsection Title'] => h1:fresh
+    p[style-name='Heading 1'] => h1:fresh
+    p[style-name='Heading 2'] => h1:fresh
+    p[style-name='Heading 3'] => h1:fresh
+    p[style-name='Heading 4'] => h1:fresh
+    p[style-name='Heading 5'] => h1:fresh
+    p[style-name='Heading 6'] => h1:fresh
+    p[style-name='Comment'] => !
+    """
+    l = []
+
+    if not path.endswith((".docx", ".docx/", ".html", ".html/")):
+        raise ValueError
+
+    elif path.endswith((".docx", ".docx/")):
+        # first, lets find all the titles and replace then with separators "///"
+        with open(path, "rb") as docx_file:
+            result = mammoth.convert_to_html(docx_file, style_map=style_map)
+            html = result.value
+            html = re.sub(r'<a(\w|"|=| )+></a>', "", html)
+
+    splitted = re.split("<h1>", html)
     
+    for s in splitted:
+        if match := re.search(r'^(.+)</h1>(.*)', s):
+            title, body = match.groups()
+            body = re.sub("</p>", "\n", re.sub(r'<p>', "", body))
+            l.append(create_chapter(title, body))
+    return l
+
+        
+def get_meta():
+    print("\n\n    Now let's add some information about your book!\n\n")
+
+    # empty fields here will be ignored in the future
+    meta = {
+        "title": "",
+        "author": "",
+        "date": "",
+        "contact": "",
+        "license": "",
+        "quote": "",
+        "bio": ""
+    }       
+
+    for key in meta:
+        meta[key] = input(f"Add {key}: ")
+
+    return meta
+
+
+def create_chapter(title: str, body: str):
+    return Chapter(title, body)
+
+
+
 
 if __name__ == "__main__":
     main()
