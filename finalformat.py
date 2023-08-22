7e69427e1e72d202c28a70cb5bf0ce8aa12c8d82
Gabriel Magalhães <gabm.motion@gmail.com>
2023-08-22 16:19:25 -0300

    Melhorias gerais e fontes

--- finalformat.py
+++ finalformat.py
@@ -50,13 +50,19 @@ class PDF(FPDF):
     def __init__(self, orientation, unit, format):
         super().__init__(orientation, unit, format)
         self.footer_state = False
+        self.font1 = ""
+        self.font1_path = ""
+        self.font1_italic = ""
+        self.font2 = ""
+        self.font2_path = ""
+        self.indent = 6
 
     def footer(self):
         if self.footer_state:
             # Go to 1 cm from bottom
             self.set_y(-12)
             # Select Arial italic 8
-            self.set_font('Helvetica', '', 6)
+            self.set_font(self.font1, "", 6)
             # Print page number
             if self.page_no() % 2 == 0:
                 place = "L"
@@ -64,11 +70,53 @@ class PDF(FPDF):
                 place = "R"
             self.cell(0, 10, '%s' % self.page_no(), 0, 0, place)
 
+    # def multi_cell(self, w, h, txt='', border=0, align='J', fill=0, split_only=False):
+    #     ...
+
+
+    def assign_fonts(self, font1, font2):
+        match font1:
+            case "0":
+                self.font1 = "PT_Serif"
+                self.font1_path = "./fonts/PT_Serif/PTSerif-Regular.ttf"
+                self.font1_italic = "./fonts/PT_Serif/PTSerif-Italic.ttf"
+            case "1":
+                self.font1 = "EB_Garamond"
+                self.font1_path = "./fonts/EB_Garamond/EBGaramond-Regular.ttf"
+                self.font1_italic = "./fonts/EB_Garamond/EBGaramond-Italic.ttf"
+            case "2":
+                self.font1 = "Fira_Sans"
+                self.font1_path = "./fonts/Fira_Sans/FiraSans-Regular.ttf"
+                self.font1_italic = "./fonts/Fira_Sans/FiraSans-Italic.ttf"
+            case _:
+                self.font1 = "PT_Serif"
+                self.font1_path = "./fonts/PT_Serif/PTSerif-Regular.ttf"
+                self.font1_italic = "./fonts/PT_Serif/PTSerif-Italic.ttf"
+
+        match font2:
+            case "0":
+                self.font2 = "Playfair"
+                self.font2_path = "./fonts/Playfair_Display/PlayfairDisplay-Medium.ttf"
+            case "1":
+                self.font2 = "DMSans"
+                self.font2_path = "./fonts/DM_Sans/DMSans-Medium.ttf"
+            case "2":
+                self.font2 = "Raleway"
+                self.font2_path = "./fonts/Raleway/Raleway-Medium.ttf"
+            case "3":
+                self.font2 = "PlayfairSC"
+                self.font2_path = "./fonts/Playfair_Display_SC/PlayfairDisplaySC-Regular.ttf"
+            case _:
+                self.font2 = "Playfair"
+                self.font2_path = "./fonts/Playfair_Display/PlayfairDisplay-Medium.ttf"
+
+
 
 # Global variables
 INDENTATION_SPACES = 4
 SMALL_SIZE = (125, 180)
 REGULAR_SIZE = (140, 210)
+LINE_SPACING = 4.6
 
 
 def main():
@@ -76,15 +124,15 @@ def main():
     parser = argparse.ArgumentParser(
         prog="finalformat",
         description="This program transforms docx and html files into formatted pdfs",
-        epilog="Remember to input a docx or html file with a Title and Paragraphs sctructure!\n",
+        epilog="Remember to input a docx or html file with a Title followed by Paragraphs sctructure!\n",
     )
 
     # add arguments to parser
-    parser.add_argument("path", type=str, help="path to the file you want to format into pdf")
-    parser.add_argument("-s", "--size", type=str, help="size of the page: expected Regular or Small. If ommitted, defaults to Regular")
-    parser.add_argument("-f", "--font_body", type=str, help="Pick one of the available fonts. 0: Helvetica; 1: Times New Roman. Defaults to 1.")
-    parser.add_argument("-t", "--font_title", type=str, help="0: <same font as font_body>; 0: Helvetica, 1: Times New Roman. Defaults to 0.")
-    parser.add_argument("-ta", "--title_align", type=str, help="write L for Left, R for Right, C for Center alignment of the title")
+    parser.add_argument("path", type=str, help="path to the docx or html file you want to format into pdf")
+    parser.add_argument("-s", "--size", type=str, help="size of the page: expected Regular or Small. If omitted, defaults to Regular")
+    parser.add_argument("-f", "--font_body", type=str, help="Pick one of the available fonts. 0: PT Serif; 1: EB Garamond; 2: Fira Sans. Defaults to 0 if omitted.")
+    parser.add_argument("-t", "--font_title", type=str, help="0: Playfair Display; 1: DM Sans; 2: Raleway; 3: Playfair Display SC. Defaults to 0 if omitted.")
+    parser.add_argument("-ta", "--title_align", type=str, help="write L for Left, R for Right, C for Center alignment of the title. Defaults to Left")
 
     # read the arguments into variable
     args = parser.parse_args()
@@ -93,7 +141,7 @@ def main():
         # returns list of Chapter objects
         chapters = read_file(args.path)
     except ValueError:
-        sys.exit("File extension not supported! Try html or docx file with title and paragraphs")
+        sys.exit("File extension not supported! Try html or docx file")
         
     # call function to add metadata (title, author, year, contact info, copywright, quote, bio) prompting the user first
     meta = get_meta()
@@ -114,7 +162,7 @@ def main():
     # ask the user if he/she wants to change the order of chapters. If so, call change_index() to sort chapters in Book instance
     while True:
         try:
-            answer = input("Would you like to change the order of chapters(Y/N):").lower()
+            answer = input("\nWould you like to change the order of chapters(Y/N):").lower()
         except EOFError:
             sys.exit("Program interruptef by keyboard.")
 
@@ -137,6 +185,9 @@ def main():
 
 
 def read_file(path: str):
+    """
+    Reads the docx or html file and separates it into several strings (Title and Body), returning a list of Chapter objects.
+    """
     # variable for mammoth html convertion
     style_map = """
     p[style-name='Title'] => h1:fresh
@@ -171,8 +222,8 @@ def read_file(path: str):
     for s in splitted:
         if match := re.search(r'^(.+)</h1>(.*)', s):
             title, body = match.groups()
-            body = re.sub("</p>", "\n", re.sub(r'<p>', "", body))
-            body = replace_tags(body).replace("\n", f"\n{' ' * INDENTATION_SPACES}")
+            body = re.sub("</p>", "\n", re.sub(r'<p>', "", body)).strip()
+            body = " " * INDENTATION_SPACES + replace_tags(body).replace("\n", "\n" + " " * INDENTATION_SPACES)
             l.append(Chapter(title, body))
     return l
 
@@ -238,6 +289,7 @@ def replace_tags(string):
 def consolidate_pdf(book, path, title_align):
     # create PDF instance
     pdf = PDF('P', 'mm', book.size)
+
     # set margins and page break
     m = book.size[0] / 8.5
     pdf.set_margins(m, m)
@@ -250,8 +302,13 @@ def consolidate_pdf(book, path, title_align):
         #default title alignment
         title_align = "L"
 
-    # call match fonts to consolidate fonts
-    font, font2 = match_fonts(book)
+    # call assign fonts to consolidate fonts
+    pdf.assign_fonts(book.font1, book.font2)
+
+    # load custom fonts into memory
+    load_fonts(pdf)  
+    font = pdf.font1
+    font2 = pdf.font2
 
     # small title page
     pdf.add_page()
@@ -278,7 +335,6 @@ def consolidate_pdf(book, path, title_align):
     # add finalformat. logo
     pdf.set_y(book.size[1])
     pdf.image("png/FinalFormat..png", x=book.size[0] - m - 30 , y=10 + book.size[1] / 2, w=30, h=0, type="PNG")
-
 
     # create page for metadata, formating a string from data
     data = f"    {book.title} / {book.author} ; \npowered by finalformat.\n\n    {book.date}\n    {book.contact}\n{book.license}\n"
@@ -307,13 +363,16 @@ def consolidate_pdf(book, path, title_align):
         pdf.set_y(4 + book.size[1] / 2)
         pdf.cell(0, 3, author, align="R")
 
+    # empty page
+    if pdf.page_no() % 2:
+        pdf.add_page()
 
-    # creat body of text from chapters. 
+    # creat body of text from chapters. Always start at an odd number
     for chapter in book.index:
-        # make title cell
         pdf.add_page()
         # set footer_state as True so the numbers appear in the footer
         pdf.footer_state = True
+        # make title cell
         pdf.set_font(font2, "", 20)
         pdf.cell(0, 0, chapter.title, align=title_align)
 
@@ -321,7 +380,8 @@ def consolidate_pdf(book, path, title_align):
         pdf.set_x(0)
         pdf.set_y(60)
         pdf.set_font(font, "", 8)
-        pdf.multi_cell(0, 5.2, chapter.body)
+        pdf.multi_cell(0, LINE_SPACING, chapter.body)
+
 
     pdf.add_page()
     pdf.footer_state = False
@@ -337,9 +397,9 @@ def consolidate_pdf(book, path, title_align):
 
     # add bio page at the end (counter-folder)
     pdf.add_page()
-    pdf.set_font(font2, "", 7)
+    pdf.set_font(font, "", 7)
     pdf.set_y(book.size[1] / 3)
-    pdf.multi_cell(book.size[0] - (m + 10), 3, book.bio, align="C")
+    pdf.multi_cell(0, 3, book.bio, align="C")
 
     pdf.set_y(20 + book.size[1] / 2)
     pdf.cell(0, 3, "Published in " + book.date, align="C")
@@ -356,23 +416,18 @@ def consolidate_pdf(book, path, title_align):
     return True
 
 
-def match_fonts(book):
-    match book.font1:
-        case "0":
-            font = "Helvetica"
-        case "1":
-            font = "Times"
-        case _:
-            font = "Times"
+def load_fonts(pdf):
+    """
+    Loads the custom fonts into memory, exiting in case they are not found
+    """
+    try:
+        pdf.add_font(pdf.font1, "", pdf.font1_path, uni=True)
+        pdf.add_font(pdf.font1, "I", pdf.font1_italic, uni=True)
+        pdf.add_font(pdf.font2, "", pdf.font2_path, uni=True)
+    except RuntimeError:
+        sys.exit("Unable to load fonts")
 
-    match book.font2:
-        case "0":
-            font2 = "Helvetica"
-        case "1":
-            font2 = "Times"
-        case _:
-            font2 = "Helvetica" 
-    return [font, font2]
+    return True
 
 
 if __name__ == "__main__":
